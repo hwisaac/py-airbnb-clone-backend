164f373ba0340fe490bb154130085f771a0f8a88
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-08-18 17:09:36 +0900

    upload photo

--- medias/models.py
+++ medias/models.py
@@ -4,7 +4,7 @@ from common.models import CommonModel
 
 class Photo(CommonModel):
 
-    file = models.ImageField()
+    file = models.URLField()
     description = models.CharField(
         max_length=140,
     )
